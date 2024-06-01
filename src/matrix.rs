f14cab87d5c15f62199caddae8236cf1dbe0cea3
Michael Dodson <MichaelJDodson42@gmail.com>
2024-06-01 18:17:58 -0400

    Completed basic data retrieval functions

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,31 @@
 
 
-pub struct Matrix<T, const N: usize> {
-    data: [[T; N]; N],
+pub struct Matrix<const N: usize, const M: usize> {
+    data: [[f64; M]; N],
 }
 
-impl<T, const N: usize> Matrix<T,N>{
+impl<N,M> Matrix<N,M>{
     
+    // return a specific element
+    fn element(&self, row: usize, column: usize) -> f64 {
+        return self.data[row][column];
+    }
+
+    // return a whole row of data within the matrix
+    fn row(&self, row: usize) -> [f64] {
+        return self.data[row];
+    }
+
+    // return a column of data from within the matrix
+    fn column(&self, column: usize) -> [f64] {
+        let column_data: [f64; self.data.len()] = [0; self.data.len()];
+
+        for i in 0..self.data.len() {
+            column_data[i] = self.data[i][column];
+        }
+
+        return column_data;
+    }
+
 }
+
--- src/operations.rs
+++ src/operations.rs
@@ -1,3 +1,5 @@
 mod operations {
-    
+    fn matrix_addition() {
+        
+    }
 }
